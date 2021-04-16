1b98508b16f351bda0b4427b332ea6744523f304
Kyle <kyle.e.cardwell@gmail.com>
2021-04-15 19:43:39 -0600

    Passing 43 of 55 tests, completed Tasks 1-4  & part of 5

--- index.js
+++ index.js
@@ -92,12 +92,12 @@ class Airplane {
     }
 
     drive(distance) {
-      let toDrive = this.tank * this.milesPerGallon;
+      let distToEmpty = this.tank * this.milesPerGallon;
       
       for (let i = distance; i > 0; i-- ) {        
-        toDrive -= 1;
+        distToEmpty -= 1;
         this.odometer += 1;
-        this.tank = toDrive/this.milesPerGallon;
+        this.tank = distToEmpty/this.milesPerGallon;
         if (this.tank <= 0) {
           console.log(`I ran out of fuel at ${this.odometer} miles!`)
           return `I ran out of fuel at ${this.odometer} miles!`
@@ -134,6 +134,8 @@ class Airplane {
       return `Hello my name is ${this.name}, I am from ${this.location}`
     }
 
+    
+
   }
   
   /*
@@ -150,9 +152,24 @@ class Airplane {
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
- class Instructor {
+ class Instructor extends Lambdasian {
+    constructor(attrs) {
+      super(attrs);
+      this.specialty = attrs.specialty;
+      this.favLanguage = attrs.favLanguage;
+      this.catchPhrase = attrs.catchPhrase;
+    }
 
+    demo(subject) {
+      return `Today we are learning about ${subject}`
+    }
+
+    grade(student, subject) {
+      return `${student.name} receives a perfect score on ${subject}`
+    }
  }
+
+ 
   /*
     TASK 5
       - Write a Student class extending Lambdasian.
@@ -168,7 +185,22 @@ class Airplane {
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
- class Student {
+ class Student extends Lambdasian {
+   constructor(attrs) {
+     super(attrs);
+     this.previousBackground = attrs.previousBackground;
+     this.className = attrs.className;
+     this.favSubjects = attrs.favSubjects;
+   }
+
+   listSubjects() {
+     console.log(`Loving ${this.favSubjects.join(", ")}!`);
+     return `Loving ${this.favSubjects.join(", ")}!`;
+   }
+
+   PRAssignment(subject) {
+     return `${this.name} has submitted a PR for ${subject}`
+   }
      
  }
   
